7b6bf3b73bb6585cd0df930aae54e63f3359207a
Aditya Borgaonkar <borg.aditya@gmail.com>
2019-09-12 16:51:50 +0530

    Updated README.md

--- app/views.py
+++ app/views.py
@@ -15,37 +15,21 @@ def predict(request):
     quandl.ApiConfig.api_key = "RHVBxuQQR_xxy8SPBDGV"
     ticker_value = request.POST.get('ticker')
     number_of_days = request.POST.get('days')
-    number_of_days = int(number_of_days)
-    i = 0
-    
+    number_of_days = int(number_of_days) 
     df = quandl.get("WIKI/"+ticker_value+"")
     df = df[['Adj. Close']]
-
-    print(df.tail())
-
     forecast_out = int(number_of_days)
-
     df['Prediction'] = df[['Adj. Close']].shift(-forecast_out)
-
     X = np.array(df.drop(['Prediction'],1))
     X = preprocessing.scale(X)
-
     X_forecast = X[-forecast_out:]
     X = X[:-forecast_out]
-
     y = np.array(df['Prediction'])
     y = y[:-forecast_out]
-
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.2)
-
     clf = LinearRegression()
     clf.fit(X_train,y_train)
-
     confidence = clf.score(X_test, y_test)
-    print("confidence: ", confidence)
-
     forecast_prediction = clf.predict(X_forecast)
-    print(forecast_prediction)
     forecast = forecast_prediction.tolist()
-    d = dict(enumerate(forecast_prediction.flatten(), 1))
-    return render(request,'index.html',{'forecast_prediction' : forecast_prediction,'confidence' : confidence,'forecast': forecast,'ticker_value':ticker_value,'number_of_days':number_of_days})
+    return render(request,'index.html',{'confidence' : confidence,'forecast': forecast,'ticker_value':ticker_value,'number_of_days':number_of_days})
